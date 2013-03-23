4a271d49f97954e0cb245421f50e3d7d8beda266
Wayne Warren <waynr@sdf.org>
2013-03-23 15:13:26 -0500

    Initial version of "backup_dots" and "install_dots" working.

--- lib/bash/dots.functions.sh
+++ lib/bash/dots.functions.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
 
-backup_dots() {
-	#---------------------------------------
-	# Remove and backup current files, then create symlink to new file.
-	#
-	mkdir -p ${backup_dir_time}
+set_var() {
+	local var_name=${1:?}
+	local var_value=${2:?}
+	local filename=${3:?}
 
-	date=`date --rfc-3339=date`
-	time=`date --rfc-3339=seconds | \
-		awk '{print $s}'`
-	backup_dir="${FORKMYDOTS_DIR}/backups"
-	#[ ! -h "${file}" ] && cp "${file}" "${backup_dir_time}/${file}"
+	sed -i -e \
+		"s|^${var_name}=.*|${var_name}=\"${var_value}\"|" \
+		"${filename}"
 }
 
+get_dot_files() {
+	find "${1:?}" \
+		-mindepth 1 \
+		-type f \
+		! -name "*.swp" \
+		-printf "%P "
+}
+
+backup_dots() {
+	local backup_dir="${forkmydots_dir}/backups"
+	local date=`date --rfc-3339=date`
+	local time=`date --rfc-3339=seconds | \
+		awk '{print $2}'`
+	backup_dir="${backup_dir}/${date}/${time}/"
+	mkdir -p ${backup_dir}
+
+	local file_list=`get_dot_files ${1:?}`
+	for file in ${file_list} ;do
+		local dot_file=".${file}"
+		[ ! -h "${dot_file}" ] && cp "${dot_file}" "${backup_dir}/${file}"
+	done
+}
+
+
 link_dots() {
-	specified_dir=${1:?}
-
-	#---------------------------------------
-	# Obtain list of files using names that are relatively correct for the current
-	# user's home directory.
-	#
-
-	file_list=`find "${specified_dir}" -mindepth 1 -type f -printf "%P "`
+	local dot_dir="${1:?}"
+	local file_list=`get_dot_files ${dot_dir:?}`
 
 	for file in ${file_list} ;do
-		ln -sf "${PWD}/${specified_dir}/${file}" ".${file}"
+		ln -sf "${PWD}/${dot_dir}/${file}" ".${file}"
+		set_var "FORKMYDOTS_DIR" "${forkmydots_dir}" ".${file}"
 	done 
 }
 
